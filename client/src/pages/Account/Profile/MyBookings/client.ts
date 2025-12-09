a31132075d6e125e30495f9473f61fe287ccd5a2
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-12-09 10:49:03 -0500

    Added with credentials to all axios

--- client/src/pages/Account/Profile/MyBookings/client.ts
+++ client/src/pages/Account/Profile/MyBookings/client.ts
@@ -7,7 +7,7 @@ const HTTP_SERVER = import.meta.env.VITE_API_URL || "http://localhost:4000";
 const TURFS_API = `${HTTP_SERVER}/api/turfs`;
 
 export const fetchAllTurfs = async () => {
-  const { data } = await axios.get(TURFS_API);
+  const { data } = await axiosWithCredentials.get(TURFS_API);
   return data;
 };
 
--- client/src/pages/Account/client.ts
+++ client/src/pages/Account/client.ts
@@ -45,33 +45,33 @@ export const findAllUsers = async () => {
 
 export const findUsersByRole = async (role: string) => {
   const response = await
-    axios.get(`${USERS_API}?role=${role}`);
+    axiosWithCredentials.get(`${USERS_API}?role=${role}`);
   return response.data;
 };
 
 export const findUsersByPartialName = async (name: string) => {
   const response = await
-    axios.get(`${USERS_API}?name=${name}`);
+    axiosWithCredentials.get(`${USERS_API}?name=${name}`);
   return response.data;
 };
 
 export const findUserById = async (id: string) => {
   const response = await
-    axios.get(`${USERS_API}/${id}`);
+    axiosWithCredentials.get(`${USERS_API}/${id}`);
   return response.data;
 };
 
 export const deleteUser
   = async (userId: string) => {
     const response = await
-      axios.delete( `${USERS_API}/${userId}` );
+      axiosWithCredentials.delete( `${USERS_API}/${userId}` );
     return response.data;
 };
 
 export const createUser =
   async (user: any) => {
     const response = await
-      axios.post(`${USERS_API}`, user);
+      axiosWithCredentials.post(`${USERS_API}`, user);
     return response.data;
 };
 
